d8b476e03414d177e9b3b964e22d33422ac85fd9
Konstantin Kutsyllo <trikadin@mail.ru>
2015-04-04 23:48:43 +0300

    Теперь музыка сохраняется с правильными названиями

---
+++ background.js
@@ -0,0 +1,11 @@
+/**
+ * @fileOverview , date 04.04.15:
+ * @author <trikadin@mail.ru>
+ */
+
+chrome.runtime.onMessage.addListener(
+    function(msg){
+        document.body.innerHTML= msg;
+        document.body.firstChild.click();
+    }
+);
--- content.js
+++ content.js
@@ -1,48 +1,76 @@
+document.body.addEventListener(chrome.runtime.id, function(e){
+    chrome.runtime.sendMessage(e.detail);
+}, false);
+
 function executeInBody(func) {
     var scr = document.createElement("script");
-    scr.text= "(" + func.toString() + ")()";
+    scr.text = "(" + func.toString() + ")('" + chrome.runtime.id + "')";
     document.querySelector("body").appendChild(scr);
 }
 
-executeInBody(function() {
+executeInBody(
+    function (extensionId) {
+        function trim(string) {
+            var str = string.replace(/^\s\s*/, ''),
+                ws = /\s/,
+                i = str.length;
+            while (ws.test(str.charAt(--i)));
+            return str.slice(0, i + 1);
+        }
 
-    function newSaver(/*String*/ audioUrl) {
-        var wrap = document.createElement("div");
-        wrap.className = "audio_save_wrap fl_r";
-        wrap.onmouseover = function () {
+        function showTip() {
             Audio.rowActive(this, "Сохранить аудиозапись", [9, 5, 0]);
-        };
-        wrap.onmouseout = function () {
+        }
+
+        function hideTip() {
             Audio.rowInactive(this);
-        };
-        wrap.onclick = function (e) {
+        }
+
+        function sendMessage(message) {
+            var event= new CustomEvent(extensionId, {detail: message});
+            document.body.dispatchEvent(event);
+        }
+
+        function save(e) {
             e.stopPropagation();
-        };
-        var saver = document.createElement("a");
-        saver.setAttribute("download","huynya.mp3");
-        saver.className = "audio_save";
-        saver.href = audioUrl+"?/huynya.mp3";
-        wrap.appendChild(saver);
-        return wrap;
-    }
-
-    function saveAll(count) {
-
-    }
-
-    setInterval(function () {
-        if (location.pathname.indexOf("/audio") !== 0) {
-            return
+            e.preventDefault();
+            sendMessage(this.parentNode.innerHTML);
         }
-        var audios = document.querySelectorAll("#audio.new .audio");
-        for (var i = 0; i !== audios.length; ++i) {
-            if (audios[i].querySelector(".audio_save")) {
-                continue;
+
+        function addSaver(audio) {
+            if (audio.querySelector(".audio_save")) {
+                return
             }
-            audios[i].querySelector(".actions").appendChild(
-                newSaver(audios[i].querySelector(".play_btn input").value)
-            )
-        }
-    }, 300);
 
-});
+            var url= audio.querySelector(".play_btn input").value,
+                title= trim(audio.querySelector(".title_wrap").textContent);
+                wrapper = document.createElement("div");
+            wrapper.className = "audio_save_wrap fl_r";
+            wrapper.addEventListener('mouseover', showTip, false);
+            wrapper.addEventListener('mouseout', hideTip, false);
+
+            var saver = document.createElement("a");
+            saver.setAttribute("download", title + '.mp3');
+            saver.className = "audio_save";
+            saver.href = url;
+            saver.addEventListener('click', save, false);
+            wrapper.appendChild(saver);
+            audio.querySelector(".actions").appendChild(wrapper)
+        }
+
+        function saveAll(count) {
+
+        }
+
+        setInterval(
+            function () {
+                if (location.pathname.indexOf("/audio") !== 0) {
+                    return
+                }
+                var audios = [].concat.apply([], document.querySelectorAll("#audio.new .audio"));
+                audios.forEach(addSaver);
+            }, 300
+        );
+
+    }
+);
